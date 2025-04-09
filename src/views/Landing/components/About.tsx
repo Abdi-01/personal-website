348ee89e820e258bddbb10113383e782f7000c37
Abdi-01 <abdialghi@gmail.com>
2025-04-09 15:09:47 +0700

    revamp: about

--- src/views/Landing/components/About.tsx
+++ src/views/Landing/components/About.tsx
@@ -6,48 +6,49 @@ import Container from "@/components/core/Container";
 
 const About = () => {
   return (
-    <Container>
-      <section id="about" className="md:flex px-8 md:px-16 py-16 items-center">
-        <div className="md:flex-1/2 space-y-7">
-          <h2 className="uppercase text-gray-500">About Me</h2>
+    // <Container>
+    <section id="about" className="bg-black">
+      <div className="container  m-auto md:flex px-8 md:px-16 py-16 items-center">
+        <div className="md:flex-1/2 space-y-7 text-white">
+          <h2 className="uppercase">About Me</h2>
           <div>
             <h2 className="text-2xl">More Than Just Code</h2>
             <p>Here&apos;s what I do:</p>
           </div>
           <div className="md:grid grid-cols-2 grid-rows-2 gap-4 py-4 space-y-5 md:space-y-0">
-            <div>
-              <BookOpen className="w-10 md:w-16 h-10 md:h-16" />
+            <div className="text-center md:text-left">
+              <BookOpen className="w-10 md:w-16 h-10 md:h-16 m-auto md:m-0 mb-6" />
               <h3 className="text-xl font-bold">Teaching is My Main Gig</h3>
-              <p className="text-sm font-light">
+              <p className="text-sm text-gray-300 font-light">
                 I spend most of my time helping aspiring developers learn
                 programming—breaking down complex concepts into something
                 practical and easy to understand.
               </p>
             </div>
-            <div>
-              <Laptop className="w-10 md:w-16 h-10 md:h-16" />
+            <div className="text-center md:text-left">
+              <Laptop className="w-10 md:w-16 h-10 md:h-16 m-auto md:m-0 mb-6" />
               <h3 className="text-xl font-bold">Building Apps on Request</h3>
-              <p className="text-sm font-light">
+              <p className="text-sm text-gray-300 font-light">
                 Outside of teaching, I take on web development projects, turning
                 ideas into functional applications that solve real problems.
               </p>
             </div>
-            <div>
-              <RocketIcon className="w-10 md:w-16 h-10 md:h-16" />
+            <div className="text-center md:text-left">
+              <RocketIcon className="w-10 md:w-16 h-10 md:h-16 m-auto md:m-0 mb-6" />
               <h3 className="text-xl font-bold">
                 Making Tech Less Intimidating
               </h3>
-              <p className="text-sm font-light">
+              <p className="text-sm text-gray-300 font-light">
                 Whether in class or through projects, I believe technology
                 should be accessible, understandable, and fun to learn.
               </p>
             </div>
-            <div>
-              <LucideHandshake className="w-10 md:w-16 h-10 md:h-16" />
+            <div className="text-center md:text-left">
+              <LucideHandshake className="w-10 md:w-16 h-10 md:h-16 m-auto md:m-0 mb-6" />
               <h3 className="text-xl font-bold">
                 Let&apos;s Learn & Build Together
               </h3>
-              <p className="text-sm font-light">
+              <p className="text-sm text-gray-300 font-light">
                 Need guidance in coding or a custom-built web app? Let’s make it
                 happen!
               </p>
@@ -61,15 +62,16 @@ const About = () => {
           >
             <Image src={Photo} alt="photo" objectFit="cover" fill />
           </div>
-          <p className="text-center font-semibold">
+          <p className="text-center text-white font-semibold">
             Abdi Alghifara Felinanda, S.Tr.T
           </p>
-          <p className="text-center font-extralight">
+          <p className="text-center text-gray-300 font-extralight">
             Fullstack Web Development Instructor
           </p>
         </div>
-      </section>
-    </Container>
+      </div>
+    </section>
+    // </Container>
   );
 };
 
