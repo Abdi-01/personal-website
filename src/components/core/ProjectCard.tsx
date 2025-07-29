6784152adb92d9e7334b6766d750d2fd258b2195
Abdi-01 <abdialghi@gmail.com>
2025-07-29 10:06:29 +0700

    revamp: portfolio

---
+++ src/components/core/ProjectCard.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "../ui/card";
+
+interface IProjectCardProps {
+  title: string;
+  description: string;
+  image: any;
+  layout: string;
+}
+
+const ProjectCard = ({
+  title,
+  description,
+  image,
+  layout,
+}: IProjectCardProps) => {
+  const isLeft = layout === "left";
+
+  return (
+    <Card
+      className={`md:w-${
+        isLeft ? "2/3" : "1/3"
+      } h-96 cursor-pointer md:relative overflow-hidden`}
+    >
+      {isLeft ? (
+        <CardContent className="md:flex h-full items-center">
+          <div className="mb-4">
+            <CardTitle className="md:text-5xl uppercase">{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+          </div>
+          <Image
+            src={image}
+            alt={title}
+            className="md:absolute top-0 -right-48 hover:-right-0 transition-all duration-500 delay-150"
+            width={650}
+            height={400}
+          />
+        </CardContent>
+      ) : (
+        <>
+          <CardHeader>
+            <CardTitle>{title}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Image
+              src={image}
+              alt={title}
+              className="m-auto"
+              width={650}
+              height={200}
+            />
+          </CardContent>
+        </>
+      )}
+    </Card>
+  );
+};
+
+export default ProjectCard;
